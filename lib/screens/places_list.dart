adc7f2c9e64d300114a7ce8ed10c863d6c95af0a
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2026-02-12 19:11:43 +0530

    added widgets->places_list.dart

--- lib/screens/places_list.dart
+++ lib/screens/places_list.dart
@@ -1,1 +1,26 @@
 // shows list of places
+
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+     const PlacesListScreen({super.key});
+
+     @override
+     Widget build(BuildContext context) {
+       return Scaffold(
+         appBar: AppBar(
+           title: const Text('Your Places'),
+           actions: [
+             IconButton(
+               icon: const Icon(Icons.add),
+               onPressed: () {},
+             ),
+           ],
+         ),
+         body: const Center(
+           child: Text('No places added yet.'),
+
+         ),
+       );
+     }
+}
