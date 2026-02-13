8bdb00c412dccad6e8ee03b843993d2dfd469d5d
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2026-02-13 11:13:20 +0530

    initialised places detail

--- lib/screens/places_detail.dart
+++ lib/screens/places_detail.dart
@@ -1,1 +1,15 @@
 //displays further detail about the place
+
+
+import '../models/place.dart';
+
+class PlaceDetailScreen extends StatelessWidget {
+  const PlaceDetailScreen({super.key});
+
+   final Place place;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
