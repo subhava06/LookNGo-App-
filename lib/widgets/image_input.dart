04719fef0240a56e109d05b2ba1e747333f9da11
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2026-02-13 11:50:06 +0530

    container-> camera icon added in image input widget

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,30 @@
+// code to:
+// allow user to open device camera
+// show a preview of the image that was taken
+//ans also pass that image back to the add place screen to add it to the list
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250,
+      width: double.infinity,
+      alignment: Alignment.center, //centres the child horz and vert inside the container
+      child: TextButton.icon(
+        icon: const Icon(Icons.camera),
+        label: const Text('Take Picture'),
+          onPressed: () {},
+      ), // it should either be a preview of the image or a button that allows us to take image
+    );
+  }
+}
