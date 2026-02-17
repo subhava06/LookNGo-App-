1f7a530766a0c8e5113360a011a2f8e72d89e55b
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2026-02-17 09:49:43 +0530

    locn widget: preview, get location

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,1 +1,31 @@
-// to add it as an element to the add place screen
+// to add it as an element to the add place screens
+
+
+import 'package:flutter/cupertino.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+        //container to show preview of the image
+        Container(
+
+        ),
+
+        Row(
+          //one button to get user location automatically
+          //second button to get loc manually
+        ),
+      ],
+    );
+  }
+}
