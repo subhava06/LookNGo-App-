03a09f3759a6fffd6a9730efb75c83deafe1f4f1
SUBHAVA OJHA <subhava.22gcebcs190@galgotiacollege.edu>
2026-02-17 10:28:11 +0530

    added functionality to add user location

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -2,6 +2,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -11,8 +12,66 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+
+   Location? _pickedLocation;
+   var _isGettingLocation = false;
+
+  //method to get current location of the user
+  void _getCurrentLocation() async {
+
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+
+    setState(() {
+      _isGettingLocation = false;
+    });
+
+    print(locationData.latitude);
+    print(locationData.longitude);
+
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      'No location chosen',
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+        color: Theme.of(context).colorScheme.onSurface,
+      ),
+    );
+
+    if(_isGettingLocation) {
+      previewContent =const CircularProgressIndicator();
+    }
+
     return Column(
       children: [
 
@@ -27,12 +86,7 @@ class _LocationInputState extends State<LocationInput> {
                 color: Theme.of(context).colorScheme.primary.withValues(alpha: 0.2)),
           ),
           // to get snapshot of the selected image
-          child: Text(
-            'No location chosen',
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onSurface,
-            ),),
+          child: previewContent,
     ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -42,7 +96,7 @@ class _LocationInputState extends State<LocationInput> {
             TextButton.icon(
               icon: const Icon(Icons.location_on),
               label: const Text('Get User Location'),
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
             ),
 
             TextButton.icon(
